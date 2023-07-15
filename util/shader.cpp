9c59fb5728c9ba115c44495ec7f0d36e545b256b
Philip Caplan <caplan.philip@gmail.com>
2023-07-15 09:20:45 -0400

    move some utilities to cpp implementation file

---
+++ util/shader.cpp
@@ -0,0 +1,216 @@
+//
+//  wings: web interface for graphics applications
+//
+//  Copyright 2023 Philip Claude Caplan
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+#include "shader.h"
+
+#include "log.h"
+
+namespace wings {
+
+namespace {
+std::string get_shader(const std::string& filename) {
+  std::string content;
+  std::ifstream file_stream(filename.c_str(), std::ios::in);
+
+  ASSERT(file_stream.is_open()) << "could not read file " << filename;
+
+  std::string line = "";
+  while (!file_stream.eof()) {
+    std::getline(file_stream, line);
+    content.append(line + "\n");
+  }
+
+  file_stream.close();
+  return content;
+}
+}  // namespace
+
+ShaderProgram::ShaderProgram() {
+  handle_ = glCreateProgram();
+  ASSERT(handle_ >= 0);
+}
+
+void ShaderProgram::set_source(const std::string& dir, const std::string& name,
+                               bool with_geometry, bool with_tessellation,
+                               const std::vector<std::string>& macros) {
+  macros_ = macros;
+
+  std::string base = dir + name;
+
+  // compile the program using each stage, and then link
+  std::string vs = get_shader(base + "-vs.glsl");
+  std::string fs = get_shader(base + "-fs.glsl");
+  std::string gs, tcs, tes;
+  if (with_geometry) gs = get_shader(base + "-gs.glsl");
+  if (with_tessellation) {
+    tcs = get_shader(base + "-tcs.glsl");
+    tes = get_shader(base + "-tes.glsl");
+  }
+  compile(vs, fs, gs, tcs, tes);
+}
+
+void ShaderProgram::compile(const std::string& src, GLenum type) {
+  ASSERT(handle_ >= 0);
+
+  GLuint shader = glCreateShader(type);
+
+  std::string source;
+  if (macros_.size() > 0) {
+    std::size_t idx = src.find("#version");
+    idx = src.find("\n");
+
+    std::string source1 = "//" + src.substr(0, idx);  // comment out version
+    std::string source2 = "\n";
+    for (auto& s : macros_) source2 += s + "\n";
+    std::string source3 = src.substr(idx + 1, src.size());
+    source = source1 + source2 + source3;
+  } else
+    source = src;
+
+  const auto* c = source.data();
+  GL_CALL(glShaderSource(shader, 1, &c, NULL));
+  GL_CALL(glCompileShader(shader));
+
+  // check for errors
+  int result;
+  GL_CALL(glGetShaderiv(shader, GL_COMPILE_STATUS, &result));
+  if (result == GL_FALSE) {
+    int length = 0;
+    GL_CALL(glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length));
+    if (length > 0) {
+      std::string log(length, ' ');
+      int written = 0;
+      GL_CALL(glGetShaderInfoLog(shader, length, &written, &log[0]));
+      std::cout << log << std::endl;
+    }
+    // std::cout << "shader source:\n" << source << std::endl;
+  }
+  ASSERT(result == GL_TRUE) << "failed to compile shader stage " << type;
+
+  // compile succeeded, attach shader to program
+  GL_CALL(glAttachShader(handle_, shader));
+}
+
+void ShaderProgram::compile(const std::string& vs, const std::string& fs,
+                            const std::string& gs, const std::string& tcs,
+                            const std::string& tes) {
+  // compile each shader stage
+  compile(vs, GL_VERTEX_SHADER);
+  compile(fs, GL_FRAGMENT_SHADER);
+  if (!gs.empty()) compile(gs, GL_GEOMETRY_SHADER);
+  if (!tcs.empty()) {
+    ASSERT(!tes.empty());
+    compile(tcs, GL_TESS_CONTROL_SHADER);
+    compile(tes, GL_TESS_EVALUATION_SHADER);
+  }
+
+  // link the program
+  GL_CALL(glLinkProgram(handle_));
+
+  int status = 0;
+  GL_CALL(glGetProgramiv(handle_, GL_LINK_STATUS, &status));
+  if (status == GL_FALSE) {
+    int length = 0;
+    GL_CALL(glGetProgramiv(handle_, GL_INFO_LOG_LENGTH, &length));
+    if (length > 0) {
+      std::string log(length, '\0');
+      int written = 0;
+      GL_CALL(glGetProgramInfoLog(handle_, length, &written, &log[0]));
+      LOG << "GLSL compiler log:\n" << log;
+    }
+  }
+  ASSERT(status == GL_TRUE) << "failed to link program";
+}
+
+void ShaderProgram::use() const {
+  ASSERT(handle_ >= 0);
+  GL_CALL(glUseProgram(handle_));
+}
+
+template <>
+void ShaderProgram::set_uniform(const char* name, const vec3f& v) const {
+  GLint location = glGetUniformLocation(handle_, name);
+  if (location >= 0) GL_CALL(glUniform3f(location, v[0], v[1], v[2]));
+}
+
+template <>
+void ShaderProgram::set_uniform(const char* name, const vec2f& v) const {
+  GLint location = glGetUniformLocation(handle_, name);
+  if (location >= 0) GL_CALL(glUniform2f(location, v[0], v[1]));
+}
+
+template <>
+void ShaderProgram::set_uniform(const char* name, const int& x) const {
+  GLint location = glGetUniformLocation(handle_, name);
+  if (location >= 0) GL_CALL(glUniform1i(location, x));
+}
+
+template <>
+void ShaderProgram::set_uniform(const char* name, const float& x) const {
+  GLint location = glGetUniformLocation(handle_, name);
+  if (location >= 0) GL_CALL(glUniform1f(location, x));
+}
+
+template <>
+void ShaderProgram::set_uniform(const char* name, const mat4f& m) const {
+  GLint location = glGetUniformLocation(handle_, name);
+  if (location >= 0)
+    GL_CALL(glUniformMatrix4fv(location, 1, GL_FALSE, &m(0, 0)));
+}
+
+void ShaderLibrary::create() {
+  std::string version = "#version " +
+                        std::to_string(WINGS360_GL_VERSION_MAJOR) +
+                        std::to_string(WINGS360_GL_VERSION_MINOR) + "0";
+  add("points", "points", false, false, {version, "#define WITH_GS 0"});
+  add("nodes", "points", true, false, {version, "#define WITH_GS 1"});
+  add("edges-q1-p0", "edges", true, false, {version, "#define ORDER 0"});
+  add("triangles-q1-p0", "triangles", true, false,
+      {version, "#define ORDER 0"});
+  add("triangles-q1-p1", "triangles", true, false,
+      {version, "#define ORDER 1"});
+  add("triangles-q1-pt", "triangles", true, false,
+      {version, "#define ORDER -1"});
+  add("quads-q1-p0", "quads", true, false, {version, "#define ORDER 0"});
+  add("quads-q1-p1", "quads", true, false, {version, "#define ORDER 1"});
+  add("quads-q1-pt", "quads", true, false, {version, "#define ORDER -1"});
+  add("polygons-q1-p0", "polygons", true, false, {version, "#define ORDER 0"});
+  add("prisms-q1-p0", "prisms", true, false, {version, "#define ORDER 0"});
+  add("prisms-q1-p1", "prisms", true, false, {version, "#define ORDER 1"});
+  add("pyramids-q1-p0", "pyramids", true, false, {version, "#define ORDER 0"});
+  add("pyramids-q1-p1", "pyramids", true, false, {version, "#define ORDER 1"});
+  add("tetrahedra-q1-p0", "tet", true, false, {version, "#define ORDER 0"});
+  add("tetrahedra-q1-p1", "tet", true, false, {version, "#define ORDER 1"});
+  add("polyhedra-q1-p0", "polygons", true, false,
+      {version, "#define POLYHEDRA"});
+}
+
+void ShaderLibrary::add(const std::string& name, const std::string& prefix,
+                        bool with_geometry, bool with_tessellation,
+                        const std::vector<std::string>& macros) {
+  shaders_.insert({name, ShaderProgram()});
+  shaders_[name].set_source(base_, prefix, with_geometry, with_tessellation,
+                            macros);
+}
+
+const ShaderProgram& ShaderLibrary::operator[](const std::string& name) const {
+  ASSERT(shaders_.find(name) != shaders_.end())
+      << "could not find shader " << name;
+  return shaders_.at(name);
+}
+
+}  // namespace wings
--- util/util.h
+++ util/util.h
@@ -19,13 +19,14 @@
 
 #include <vector>
 
-#include "clip.h"
 #include "glm.h"
-#include "mesh.h"
-#include "opengl.h"
-#include "shader.h"
 
 namespace wings {
+
+class Vertices;
+template <typename T>
+class Topology;
+class ShaderProgram;
 
 struct AABB {
   AABB() {}
@@ -38,11 +39,11 @@ struct GLClipPlane;
 struct PickableObject {
   template <typename T>
   PickableObject(const Vertices& vertices, const Topology<T>& topology,
-                 index_t k, const std::string& name);
+                 uint64_t k, const std::string& name);
 
   template <typename T>
   void save_points(const Vertices& vertices, const Topology<T>& topology,
-                   index_t k);
+                   uint64_t k);
 
   double intersection(const vec3f& point, const vec3f& ray,
                       const mat4f& model_matrix) const;
@@ -55,123 +56,25 @@ struct PickableObject {
 
   std::string name;
   std::vector<vec4f> points;
-  std::vector<index_t> triangles;
-  std::vector<index_t> nodes;
-  index_t index;
+  std::vector<uint64_t> triangles;
+  std::vector<uint64_t> nodes;
+  uint64_t index;
 };
 
 struct GLClipPlane {
-  GLClipPlane() : length(-1.0f), visible(false), distance(0.0f), active(false) {
-    transformation.eye();
-  }
+  GLClipPlane();
+  ~GLClipPlane();
 
-  ~GLClipPlane() { glDeleteVertexArrays(1, &vertex_array); }
+  void initialize();
 
-  void initialize() {
-    shader.compile(detail::clip_vs, detail::clip_fs, detail::clip_gs, {}, {});
-    GL_CALL(glGenVertexArrays(1, &vertex_array));
-    GL_CALL(glGenBuffers(1, &buffer));
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat), &length,
-                         GL_STATIC_DRAW));
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, 0));
-  }
+  void define(const AABB& aabb);
 
-  void define(const AABB& aabb) {
-    center = 0.5f * (aabb.min + aabb.max);
-    vec3f dims = 1.2f * (aabb.max - aabb.min);
-    float a = std::max(dims[0], std::max(dims[1], dims[2]));
+  void get(vec3f& point, vec3f& normal) const;
 
-    vec3f u, v;
-    if (dimension == 2) {
-      u = {1, 0, 0};
-      v = {0, 1, 0};
-    } else if (dimension == 1) {
-      u = {1, 0, 0};
-      v = {0, 0, 1};
-    } else if (dimension == 0) {
-      u = {0, 1, 0};
-      v = {0, 0, 1};
-    } else
-      NOT_POSSIBLE;
-
-    coordinates[0] = 0.5f * a * (u - v);
-    coordinates[1] = 0.5f * a * (u + v);
-    coordinates[2] = -0.5f * a * (u - v);
-    coordinates[3] = -0.5f * a * (u + v);
-
-    transformation.eye();
-  }
-
-  void get(vec3f& point, vec3f& normal) const {
-    vec4f p = {coordinates[0][0], coordinates[0][1], coordinates[0][2], 1.0f};
-    p = transformation * p;  // any point is fine
-
-    vec3f u = coordinates[1] - coordinates[0];
-    vec3f v = coordinates[2] - coordinates[0];
-    normal = unit_vector(cross(u, v));
-
-    vec4f n = {normal[0], normal[1], normal[2], 0.0f};
-    n = glm::inverse(glm::transpose(transformation)) * n;
-
-    point = {p[0], p[1], p[2]};
-    normal = {n[0], n[1], n[2]};
-    normal = normal * -1.0f;
-  }
-
-  void update() {
-    // translation from the origin to the clip center,
-    // the range in the frontend code is in % of the box length
-    vec3f t = center;
-    t[dimension] += 0.01f * distance * length[dimension];
-
-    // axis of rotation, depending on the clip normal direction
-    vec3f axis;
-    if (dimension == 0)
-      axis = {1, 0, 0};
-    else if (dimension == 1)
-      axis = {0, 0, 1};
-    else if (dimension == 2)
-      axis = {0, 1, 0};
-    else
-      NOT_POSSIBLE;
-
-    // the transformation of the clip in world space is a rotation, followed by
-    // a translation
-    mat4f identity;
-    identity.eye();
-    transformation =
-        glm::translate(identity, t) * glm::rotate(identity, M_PI / 2.0, axis);
-  }
+  void update();
 
   void draw(const mat4f& model_matrix, const mat4f& view_matrix,
-            const mat4f& perspective_matrix) {
-    shader.use();
-
-    mat4f model_view_matrix = view_matrix * model_matrix * transformation;
-    mat4f mvp_matrix = perspective_matrix * model_view_matrix;
-
-    shader.set_uniform("u_ModelViewMatrix", model_view_matrix);
-    shader.set_uniform("u_ModelViewProjectionMatrix", mvp_matrix);
-
-    shader.set_uniform("u_x0", coordinates[0]);
-    shader.set_uniform("u_x1", coordinates[1]);
-    shader.set_uniform("u_x2", coordinates[2]);
-    shader.set_uniform("u_x3", coordinates[3]);
-
-    // disable culling when drawing the clipping plane, but save the original
-    // state
-    // GLboolean culling;
-    // GL_CALL(glGetBooleanv(GL_CULL_FACE, &culling));
-    // glDisable(GL_CULL_FACE);
-
-    GL_CALL(glBindVertexArray(vertex_array));
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GL_CALL(glDrawArrays(GL_POINTS, 0, 1));
-
-    // reset the culling state
-    // if (culling == GL_TRUE) glEnable(GL_CULL_FACE);
-  }
+            const mat4f& perspective_matrix);
 
   vec3f length;
   bool visible;
@@ -184,170 +87,9 @@ struct GLClipPlane {
   int dimension;
   mat4f transformation;
 
-  GLuint vertex_array;
-  GLuint buffer;
-  ShaderProgram shader;
+  int vertex_array;
+  int buffer;
+  std::shared_ptr<ShaderProgram> shader;
 };
 
-template <typename T>
-void PickableObject::save_points(const Vertices& vertices,
-                                 const Topology<T>& topology, index_t k) {
-  index = k;
-  int dim = (vertices.dim() >= 3) ? 3 : vertices.dim();
-  points.resize(topology.length(k));
-  nodes.resize(points.size());
-  for (index_t j = 0; j < topology.length(k); j++) {
-    nodes[j] = topology[k][j];
-    for (int d = 0; d < dim; d++) points[j][d] = vertices[nodes[j]][d];
-    for (int d = dim; d < 3; d++) points[j][d] = 0.0;  // in case the mesh is 2d
-    points[j][3] = 1.0;
-  }
-}
-
-template <>
-PickableObject::PickableObject(const Vertices& vertices,
-                               const Topology<Triangle>& topology, index_t k,
-                               const std::string& n)
-    : name(n) {
-  save_points(vertices, topology, k);
-  triangles = {0, 1, 2};
-}
-
-template <>
-PickableObject::PickableObject(const Vertices& vertices,
-                               const Topology<Quad>& topology, index_t k,
-                               const std::string& n)
-    : name(n) {
-  save_points(vertices, topology, k);
-  triangles = {0, 1, 2, 0, 2, 3};
-}
-
-template <>
-PickableObject::PickableObject(const Vertices& vertices,
-                               const Topology<Polygon>& topology, index_t k,
-                               const std::string& n)
-    : name(n) {
-  save_points(vertices, topology, k);
-  triangles.resize(3 * (topology.length(k) - 2));
-  int m = 0;
-  for (int j = 2; j < topology.length(k); j++) {
-    triangles[m++] = 0;
-    triangles[m++] = j - 1;
-    triangles[m++] = j;
-  }
-}
-
-template <>
-PickableObject::PickableObject(const Vertices& vertices,
-                               const Topology<Polyhedron>& topology, index_t k,
-                               const std::string& n)
-    : name(n) {
-  index = k;
-  int n_faces = topology.length(k);
-  const Topology<Polygon>& faces = topology.faces();
-
-  std::unordered_map<index_t, index_t> point_map;
-  point_map.reserve(100);
-  triangles.reserve(100);
-  for (int j = 0; j < n_faces; j++) {
-    index_t f = topology[k][j];
-    ASSERT(f >= 0);
-    int n_vertices = faces.length(f);
-    for (int i = 0; i < n_vertices; i++) {
-      index_t p = faces[f][i];
-      index_t q = -1;
-      if (point_map.find(p) == point_map.end()) {
-        nodes.push_back(p);
-        vec4f x;
-        for (int d = 0; d < 3; d++) x[d] = vertices[p][d];
-        x[3] = 1.0;
-        q = points.size();
-        points.push_back(x);
-        point_map.insert({p, q});
-      }
-    }
-
-    for (int i = 2; i < n_vertices; i++) {
-      index_t p0 = point_map.at(faces[f][0]);
-      index_t p1 = point_map.at(faces[f][i - 1]);
-      index_t p2 = point_map.at(faces[f][i]);
-
-      triangles.push_back(p0);
-      triangles.push_back(p1);
-      triangles.push_back(p2);
-    }
-  }
-}
-
-template <>
-PickableObject::PickableObject(const Vertices& vertices,
-                               const Topology<Tet>& topology, index_t k,
-                               const std::string& n)
-    : name(n) {
-  save_points(vertices, topology, k);
-  triangles = {1, 2, 3, 2, 0, 3, 0, 1, 3, 2, 1, 0};
-}
-
-template <>
-PickableObject::PickableObject(const Vertices& vertices,
-                               const Topology<Prism>& topology, index_t k,
-                               const std::string& n)
-    : name(n) {
-  save_points(vertices, topology, k);
-  triangles = {0, 1, 2, 3, 5, 4, 0, 1, 3, 1, 4, 3,
-               1, 2, 4, 2, 5, 4, 2, 0, 3, 2, 3, 5};
-}
-
-double PickableObject::intersection(int k, const vec3f& eye, const vec3f& ray,
-                                    const mat4f& model_matrix) const {
-  int i0 = triangles[3 * k];
-  int i1 = triangles[3 * k + 1];
-  int i2 = triangles[3 * k + 2];
-
-  vec4f q0 = model_matrix * points[i0];
-  vec4f q1 = model_matrix * points[i1];
-  vec4f q2 = model_matrix * points[i2];
-
-  mat3f A;
-  vec3f b;
-  for (int d = 0; d < 3; d++) {
-    A(d, 0) = q0[d] - q2[d];
-    A(d, 1) = q1[d] - q2[d];
-    A(d, 2) = -ray[d];
-    b[d] = eye[d] - q2[d];
-  }
-  vec3f c = glm::inverse(A) * b;
-  float alpha = c[0], beta = c[1], gamma = 1.0 - alpha - beta;
-  float t = c[2];
-  if (alpha < 0.0 || alpha > 1.0) return 1e20;
-  if (beta < 0.0 || beta > 1.0) return 1e20;
-  if (gamma < 0.0 || gamma > 1.0) return 1e20;
-  if (t < 0.0) return 1e20;
-  return t;
-}
-
-double PickableObject::intersection(const vec3f& eye, const vec3f& ray,
-                                    const mat4f& model_matrix) const {
-  float tmin = 1e20;
-  for (int k = 0; k < n_triangles(); k++) {
-    float tk = intersection(k, eye, ray, model_matrix);
-    if (tk < tmin) tmin = tk;
-  }
-  return tmin;
-}
-
-bool PickableObject::visible(const GLClipPlane& plane) const {
-  if (!plane.active) return true;
-
-  vec3f c, n;
-  plane.get(c, n);
-
-  for (size_t j = 0; j < points.size(); j++) {
-    vec3f p(points[j].data());
-    if (dot(p - c, n) < 1e-3) {
-      return false;
-    }
-  }
-  return true;
-}
 }  // namespace wings
